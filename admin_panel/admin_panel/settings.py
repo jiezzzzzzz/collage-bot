88e27d1ae47502a3772df99a62ab55cdcdbaba5f
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2023-04-08 11:29:16 +0300

    added requirements

--- admin_panel/admin_panel/settings.py
+++ admin_panel/admin_panel/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from environs import Env
 
-env = Env()
-env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
-
-TOKEN = env('TELEGRAM_TOKEN')
+TELEGRAM_TOKEN = '6143449818:AAH0mYRuu_oWzSA8pc3DmeDTbdQvuEYEh-Q'
+DJANGO_SECRET_KEY = 'django-insecure-voomrrfw)uu!h3+k6kdonpgv)%uopqoc=#)o3w$kt%6t_50*)&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
